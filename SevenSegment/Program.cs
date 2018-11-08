e224dd97f1572c461b2f5423342a3c641f9280b0
Luke Rolf <lukeorion0@gmail.com>
2018-11-08 00:08:23 -0600

    Part 2: variable size numbers.

--- SevenSegment/Program.cs
+++ SevenSegment/Program.cs
@@ -14,75 +14,43 @@ namespace SevenSegment
 
             do
             {
-                Console.WriteLine("Please enter a number:");
-                var input = Console.ReadLine();
+                Console.WriteLine("Please enter some numbers:");
+                var inputNumber = Console.ReadLine();
 
-                if (IsOnlyNumbers(input))
+                Console.WriteLine("Number height:");
+                var inputHeightValid = int.TryParse(Console.ReadLine(), out var inputHeight);
+
+                Console.WriteLine("Number width:");
+                var inputWidthValid = int.TryParse(Console.ReadLine(), out var inputWidth);
+
+                if (IsOnlyNumbers(inputNumber) && inputNumber != null && inputHeightValid && inputWidthValid)
                 {
-                    string sevenSegRow1 = "";
-                    string sevenSegRow2 = "";
-                    string sevenSegRow3 = "";
+                    //char[,] sevenSegDisplay;
+                    var sevenSegStrings = new List<string>();
 
-                    foreach (var number in input)
+                    foreach (var number in inputNumber)
                     {
-                        switch (number)
+                        var numberStrings = GetBigNumberStrings(number, inputHeight, inputWidth);
+
+                        if (sevenSegStrings.Count == 0) // initialize string container, but only once
                         {
-                            case '0':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "| |";
-                                sevenSegRow3 += "|_|";
-                                break;
-                            case '1':
-                                sevenSegRow1 += "   ";
-                                sevenSegRow2 += " | ";
-                                sevenSegRow3 += " | ";
-                                break;
-                            case '2':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += " _|";
-                                sevenSegRow3 += "|_ ";
-                                break;
-                            case '3':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += " _|";
-                                sevenSegRow3 += " _|";
-                                break;
-                            case '4':
-                                sevenSegRow1 += "   ";
-                                sevenSegRow2 += "|_|";
-                                sevenSegRow3 += "  |";
-                                break;
-                            case '5':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "|_ ";
-                                sevenSegRow3 += " _|";
-                                break;
-                            case '6':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "|_ ";
-                                sevenSegRow3 += "|_|";
-                                break;
-                            case '7':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "  |";
-                                sevenSegRow3 += "  |";
-                                break;
-                            case '8':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "|_|";
-                                sevenSegRow3 += "|_|";
-                                break;
-                            case '9':
-                                sevenSegRow1 += " _ ";
-                                sevenSegRow2 += "|_|";
-                                sevenSegRow3 += " _|";
-                                break;
+                            for (var i = 0; i < numberStrings.Count; i++)
+                            {
+                                sevenSegStrings.Add("");
+                            }
+                        }
+
+                        for (var i = 0; i < numberStrings.Count ; i++) // append number pieces to each string
+                        {
+                            sevenSegStrings[i] += numberStrings[i];
                         }
                     }
 
-                    Console.WriteLine(sevenSegRow1);
-                    Console.WriteLine(sevenSegRow2);
-                    Console.WriteLine(sevenSegRow3);
+                    foreach (var numberString in sevenSegStrings) // print out all the numbers!
+                    {
+                        Console.WriteLine(numberString);
+                    }
+
                     Console.WriteLine("");
                 }
                 else
@@ -93,9 +61,237 @@ namespace SevenSegment
             } while (loop);
         }
 
-        static bool IsOnlyNumbers(string testString)
+        public static bool IsOnlyNumbers(string testString)
         {
             return testString.All(c => c >= '0' && c <= '9');
+        }
+
+        public static List<string> GetNumberStrings(char number)
+        {
+            var row1 = "";
+            var row2 = "";
+            var row3 = "";
+
+            switch (number)
+            {
+                case '0':
+                    row1 += " _ ";
+                    row2 += "| |";
+                    row3 += "|_|";
+                    break;
+                case '1':
+                    row1 += "   ";
+                    row2 += " | ";
+                    row3 += " | ";
+                    break;
+                case '2':
+                    row1 += " _ ";
+                    row2 += " _|";
+                    row3 += "|_ ";
+                    break;
+                case '3':
+                    row1 += " _ ";
+                    row2 += " _|";
+                    row3 += " _|";
+                    break;
+                case '4':
+                    row1 += "   ";
+                    row2 += "|_|";
+                    row3 += "  |";
+                    break;
+                case '5':
+                    row1 += " _ ";
+                    row2 += "|_ ";
+                    row3 += " _|";
+                    break;
+                case '6':
+                    row1 += " _ ";
+                    row2 += "|_ ";
+                    row3 += "|_|";
+                    break;
+                case '7':
+                    row1 += " _ ";
+                    row2 += "  |";
+                    row3 += "  |";
+                    break;
+                case '8':
+                    row1 += " _ ";
+                    row2 += "|_|";
+                    row3 += "|_|";
+                    break;
+                case '9':
+                    row1 += " _ ";
+                    row2 += "|_|";
+                    row3 += " _|";
+                    break;
+            }
+
+            return new List<string> { row1, row2, row3 };
+        }
+
+        public static List<string> GetBigNumberStrings(char number, int height, int width)
+        {
+            var numberStrings = new List<string>();
+            var numberCode = GetNumberCode(number);
+
+            var topLine = " ";
+            for (var i = 0; i < width; i++) // construct top horizontal segment
+            {
+                if (numberCode[0, 1] != 0)
+                {
+                    topLine += "_";
+                }
+                else
+                {
+                    topLine += " ";
+                }               
+            }
+
+            topLine += " ";
+            numberStrings.Add(topLine);
+
+            for (var i = 0; i < height; i++) // construct top vertical segments
+            {
+                var upperMiddleLine = "";
+
+                if (numberCode[1, 0] != 0)
+                {
+                    upperMiddleLine += "|";
+                }
+                else
+                {
+                    upperMiddleLine += " ";
+                }
+
+                for (var j = 0; j < width; j++)
+                {
+                    upperMiddleLine += " ";
+                }
+
+                if (numberCode[1, 2] != 0)
+                {
+                    upperMiddleLine += "|";
+                }
+                else
+                {
+                    upperMiddleLine += " ";
+                }
+
+                numberStrings.Add(upperMiddleLine);
+            }
+
+            var middleLine = " ";
+            for (var i = 0; i < width; i++) // construct middle horizontal segment
+            {
+                if (numberCode[1, 1] != 0)
+                {
+                    middleLine += "_";
+                }
+                else
+                {
+                    middleLine += " ";
+                }               
+            }
+
+            middleLine += " ";
+            numberStrings.Add(middleLine);
+
+            for (var i = 0; i < height; i++) // construct bottom vertical segments
+            {
+                var lowerMiddleLine = "";
+
+                if (numberCode[2, 0] != 0)
+                {
+                    lowerMiddleLine += "|";
+                }
+                else
+                {
+                    lowerMiddleLine += " ";
+                }
+
+                for (var j = 0; j < width; j++)
+                {
+                    lowerMiddleLine += " ";
+                }
+
+                if (numberCode[2, 2] != 0)
+                {
+                    lowerMiddleLine += "|";
+                }
+                else
+                {
+                    lowerMiddleLine += " ";
+                }
+
+                numberStrings.Add(lowerMiddleLine);
+            }
+
+            var bottomLine = " ";
+            for (var i = 0; i < width; i++) // construct bottom horizontal segment
+            {
+                if (numberCode[2, 1] != 0)
+                {
+                    bottomLine += "_";
+                }
+                else
+                {
+                    bottomLine += " ";
+                }             
+            }
+
+            bottomLine += " ";
+            numberStrings.Add(bottomLine);
+
+            return numberStrings;
+        }
+
+        public static int[,] GetNumberCode(char number)
+        {
+            switch (number)
+            {
+                case '0':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 2, 0, 2 },
+                                        { 2, 1, 2 } };
+                case '1':
+                    return new int[,] { { 0, 0, 0 },
+                                        { 0, 0, 2 },
+                                        { 0, 0, 2 } };
+                case '2':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 0, 1, 2 },
+                                        { 2, 1, 0 } };
+                case '3':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 0, 1, 2 },
+                                        { 0, 1, 2 } };
+                case '4':
+                    return new int[,] { { 0, 0, 0 },
+                                        { 2, 1, 2 },
+                                        { 0, 0, 2 } };
+                case '5':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 2, 1, 0 },
+                                        { 0, 1, 2 } };
+                case '6':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 2, 1, 2 },
+                                        { 2, 1, 2 } };
+                case '7':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 0, 0, 2 },
+                                        { 0, 0, 2 } };
+                case '8':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 2, 1, 2 },
+                                        { 2, 1, 2 } };
+                case '9':
+                    return new int[,] { { 0, 1, 0 },
+                                        { 2, 1, 2 },
+                                        { 0, 1, 2 } };
+                default:
+                    return null;
+            }
         }
     }
 }
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b2d062d-32c5-4ef9-aaee-1fe1b25f22c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
